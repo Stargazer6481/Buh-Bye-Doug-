daaddf88b5b0abff38ad9adb79b2503316f389e7
Stargazer <kylehoerman@icloud.com>
2023-08-09 16:17:30 -0500

    yea

---
+++ Buh Bye Doug! Mod/Buh Bye Doug!/Buh Bye Doug!/SeeYaDoug.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEditor;
+using UnityEditor.Experimental;
+
+namespace Buh_Bye_Doug_
+{
+    public class SeeYaDoug
+    {
+        public void Start()
+        {
+            GameObject.Find("Floating Bug Holdable").SetActive(false);
+        }
+    }
+}
